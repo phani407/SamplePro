40b7638503c9f3fb9eba85243b0af6c2299f66a7
phani407 <phanibandaru23@gmail.com>
2017-12-08 17:10:00 +0530

    not

---
+++ sampleexample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  sampleexample
+//
+//  Created by SMSC MacMini2 on 08/12/17.
+//  Copyright © 2017 phanindhra B V. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        print("f")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
